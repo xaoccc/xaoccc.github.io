95e5c488005dc0a40dfaf804eb749b4c7b2b31dd
xaoccc <xaocccc@gmail.com>
2024-06-28 13:52:00 +0300

    project 14

--- project14/script.js
+++ project14/script.js
@@ -1,28 +1,43 @@
-
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    alert('teesdfsdst');
-    let inputs = this.querySelectorAll('input[required]');
-    let formIsValid = true;
-    
+function submitForm() {
+    let form = document.getElementById('myForm');
+    let inputs = form.querySelectorAll('input[required], textarea[required]');
+    let formIsValid = true;    
     
     inputs.forEach(function(input) {
-        if (!input.value) {
-            input.setCustomValidity('This field is required');
-            input.reportValidity();
+        let errorElement = document.getElementById('error-' + input.id);
+
+        if ((input.type === 'text' || input.type === 'email' || input.tagName.toLowerCase() === 'textarea') && !input.value) {
+            errorElement.textContent = 'This field is required';
+            input.classList.add('error');
             formIsValid = false;
-        } else {
-            input.setCustomValidity('');
-        }
+
+        } else if (input.type === 'radio' && !input.checked) {
+            errorElement.textContent = 'Please select a query type';
+            formIsValid = false;
+
+        } else if (input.type === 'checkbox' && !input.checked) {
+            errorElement.textContent = 'To submit this form, please consent to being contacted';
+            formIsValid = false;
+
+        } 
     });
     
-    if (!formIsValid) {
-        event.preventDefault();
-    }
-});
+}
 
-document.querySelectorAll('input[required]').forEach(function(input) {
+function validateEmail() {
+    let email = document.getElementById('email');
+    if (!email.value.includes('@')) {
+        document.getElementById('invalid-email').style.display = 'block';
+        email.classList.add('error');
+    }
+}
+
+
+
+document.querySelectorAll('input[required], textarea[required]').forEach(function(input) {
     input.addEventListener('input', function() {
         this.setCustomValidity('');
+        document.getElementById('error-' + input.id).textContent = '';
     });
 });
 

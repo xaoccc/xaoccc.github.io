ea0fabc30b2e838de2051f460dcbe9a909176971
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-08-03 14:46:39 +0300

    react init

---
+++ project24/script.js
@@ -0,0 +1,24 @@
+import '../node_modules/react/umd/react.development.js';
+import '../node_modules/react-dom/umd/react-dom.development.js';
+// import ReactDOM from 'react-dom';
+
+
+const rootDomElement = document.querySelector('main');
+
+const root = ReactDOM.createRoot(rootDomElement);
+
+function createElements(tagName) {
+    const elements = [];
+    for (let i = 1; i <= 2; i++) {
+        elements.push(React.createElement(tagName, {className: `section-${i}`, key: i}));
+    }
+    return elements;
+  }
+
+// const reactSection = React.createElement('section', {className: 'left-section'});
+
+function createDOM() {
+    return createElements('section')
+}
+
+root.render(createDOM());

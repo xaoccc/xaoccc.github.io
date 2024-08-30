4daeab1decbe1ad601eab270bdd8f3664c941fbe
xaoccc <xaocccc@gmail.com>
2024-08-30 13:25:21 +0300

    tailwind

--- project28/tailwind.config.js
+++ project28/tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+
     extend: {
-      extend: {
-        backgroundImage: {
-          'background-desktop': "url('./images/bg-desktop.svg')",
-        }
-      }
-    },
+      backgroundImage: {
+        'background-desktop': "url('./images/bg-desktop.svg')",
+      },
+      fontFamily: {
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        opensans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+      maxWidth: {
+        '80rem': '80rem',
+      },
+
+    }
+
   },
+  
   plugins: [],
 }
 

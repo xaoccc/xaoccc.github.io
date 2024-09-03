10f3dbdd1728c929a41ee3d4b11cd9e381807d43
xaoccc <xaocccc@gmail.com>
2024-09-03 17:03:07 +0300

    project 29 mobile

---
+++ project29/script.js
@@ -0,0 +1,9 @@
+function showNav() {
+  document.querySelector(".mobile-nav").style.display = "block";
+  document.querySelector(".overlay").style.display = "block";
+}
+
+function hideNav() {
+    document.querySelector(".mobile-nav").style.display = "none";
+    document.querySelector(".overlay").style.display = "none";
+  }

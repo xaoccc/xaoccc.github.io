09ceb39dc5d270dd3cc5b2ab258b67db076982d1
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-09-24 17:09:52 +0300

    desktop final

--- project30/script.js
+++ project30/script.js
@@ -2,12 +2,16 @@ const notificationsNum = document.createElement('span');
 const unreadMessages = document.querySelectorAll('.unread');
 notificationsNum.textContent = unreadMessages.length;
 const heading = document.querySelector('h1');
+const bullet = Array.from(document.querySelectorAll('.unread-bullet'));
 heading.appendChild(notificationsNum);
 
 const readAllButton = document.querySelector('.header button');
 readAllButton.addEventListener('click', (e) => {
   unreadMessages.forEach((message) => {
     message.classList.remove('unread');
+    bullet.forEach((bullet) => {
+      bullet.style.display = 'none';
+    });
   });
   notificationsNum.textContent = 0;
   e.target.textContent = 'No Unread Messages';

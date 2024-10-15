61a83b7a3b3b8006f2ce59f812f9b69c7a4ceff1
xaoccc <xaocccc@gmail.com>
2024-10-15 15:23:36 +0300

    data display test

--- project33/script.js
+++ project33/script.js
@@ -1,15 +1,38 @@
 const dataWrapper = document.querySelector('.data-wrapper');
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-weekdays.forEach(day => {
-    let graphWrapper = document.createElement('div');
-    graphWrapper.classList.add('graph');
-    let graph = document.createElement('div');
-    let label = document.createElement('p');
-    graph.classList.add('graph-data', 'graph-' + day);
-    graph.style.height = Math.floor(Math.random() * 100) + 'px';
-    label.textContent = day;
-    graphWrapper.appendChild(graph);
-    graphWrapper.appendChild(label);
-    dataWrapper.appendChild(graphWrapper);
-});
+
+
+async function fetchData() {
+    const requestURL = 'https://raw.githubusercontent.com/xaoccc/xaoccc.github.io/refs/heads/main/project33/data.json';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const data = await response.json();
+    const GRAPH_HEIGHT = 150;
+
+    const nums = data.map(entry => Number(entry.amount));
+    console.log(Math.max(...nums));
+
+    data.forEach(entry => {
+        let graphWrapper = document.createElement('div');
+        graphWrapper.classList.add('graph');
+        let graphHeading = document.createElement('div');
+        let graph = document.createElement('div');
+        let label = document.createElement('p');
+        graphHeading.classList.add('graph-heading');
+        graphHeading.textContent = entry.amount;
+        graph.classList.add('graph-data', 'graph-' + entry.day);
+         
+        graph.style.height = (Number(entry.amount) * (GRAPH_HEIGHT / Math.max(...nums))) + 'px';
+        (Number(entry.amount) === Math.max(...nums)) ? graph.style.backgroundColor = 'hsl(186, 34%, 60%)' : null;
+        label.textContent = entry.day;
+        
+        graphWrapper.appendChild(graphHeading);
+        graphWrapper.appendChild(graph);        
+        graphWrapper.appendChild(label);
+        dataWrapper.appendChild(graphWrapper);
+    });
+    
+}
+
+fetchData();
+

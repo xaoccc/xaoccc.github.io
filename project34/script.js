b7c3b800d9fd417bad3d13b2d706cf1390a71ac4
xaoccc <xaocccc@gmail.com>
2024-10-21 15:12:26 +0300

    project 34 init

--- project34/script.js
+++ project34/script.js
@@ -1,14 +1,54 @@
 const dataWrapper = document.querySelector('.data-wrapper');
 let dataSections = ['Work', 'Play', 'Study', 'Exercise', 'Social', 'Self Care'];
+const option = document.querySelector('.data-options button');
+console.log(option);
 
 dataSections.forEach(section => {
-    let section = document.createElement('div');
-    let sectionHeader = document.createElement('div');
+    let sectionWrapper = document.createElement('div');
 
-    section.classList.add('section-wrapper');
+    let sectionLogo = document.createElement('div');
+    let sectionDataWrapper = document.createElement('div');
+
+    let sectionHeader = document.createElement('div');
+    
+    sectionWrapper.classList.add('section-wrapper');
+
+    sectionLogo.classList.add('section-logo');    
+    sectionDataWrapper.classList.add('section-data-wrapper');
+
     sectionHeader.classList.add('section-header');
 
-    dataWrapper.appendChild(section);
+    switch (section) {
+        case 'Work':
+            sectionLogo.style.backgroundColor = 'hsl(15, 100%, 70%)';
+            sectionHeader.textContent = 'Work';
+            break;
+        case 'Play':
+            sectionLogo.style.backgroundColor = 'hsl(195, 74%, 62%)';
+            sectionHeader.textContent = 'Play';
+            break;
+        case 'Study':
+            sectionLogo.style.backgroundColor = 'hsl(348, 100%, 68%)';
+            sectionHeader.textContent = 'Study';
+            break;
+        case 'Exercise':
+            sectionLogo.style.backgroundColor = 'hsl(145, 58%, 55%)';
+            sectionHeader.textContent = 'Exercise';
+            break;
+        case 'Social':
+            sectionLogo.style.backgroundColor = 'hsl(264, 64%, 52%)';
+            sectionHeader.textContent = 'Social';
+            break;
+        case 'Self Care':
+            sectionLogo.style.backgroundColor = 'hsl(43, 84%, 65%)';
+            sectionHeader.textContent = 'Self Care';
+            break;
+    }
+    
+    sectionDataWrapper.appendChild(sectionHeader);
+    sectionWrapper.appendChild(sectionLogo);
+    sectionWrapper.appendChild(sectionDataWrapper);
+    dataWrapper.appendChild(sectionWrapper);
 });
 
 async function fetchData() {
@@ -17,6 +57,7 @@ async function fetchData() {
     const request = new Request(requestURL);
     const response = await fetch(request);
     let data = await response.json();
+    console.log(data);
 
 
 

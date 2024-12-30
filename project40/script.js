ff1abf52320927d41cc57423deef6f3bf52915b2
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-12-30 19:43:10 +0200

    form update

--- project40/script.js
+++ project40/script.js
@@ -10,6 +10,9 @@ const daysLeft = document.querySelector('.days-left');
 const barCurrent = document.querySelector('.bar-current');
 const overlay = document.querySelector('.overlay');
 const pledgeContainers = document.querySelectorAll('section:nth-child(3) > div');
+const fieldsets = document.querySelectorAll('fieldset');
+const rewardBtns = document.querySelectorAll('.reward');
+const submitBtns = document.querySelectorAll('button[type="submit"]');
 
 
 backedCurrent.textContent = `$${backedValue.toLocaleString('en-US')}`;
@@ -17,6 +20,13 @@ backedTotal.textContent = `of $${totalValue.toLocaleString('en-US')} backed`;
 backersTotal.textContent = backers.toLocaleString('en-US');
 daysLeft.textContent = days;
 barCurrent.style.width = `${(backedValue/totalValue)*100}%`;
+
+function resetRadioButtons() {
+    let radios = document.querySelectorAll('input[type="radio"]');
+    radios.forEach(radio => {
+        radio.checked = false;
+    });
+}
 
 pledgeContainers.forEach(pledgeContainer => {
     let pledgeButton = pledgeContainer.querySelector('button');
@@ -29,16 +39,53 @@ pledgeContainers.forEach(pledgeContainer => {
         pledgeButton.classList.remove('inactive');
         pledgeContainer.style.opacity = '1';
     }
+});
 
-    pledgeButton.addEventListener('click', () => {
-        if (pledgeButton.classList.contains('inactive')) return;
+
+rewardBtns.forEach(rewardBtn => {    
+    rewardBtn.addEventListener('click', () => {        
+        if (rewardBtn.classList.contains('inactive')) return;
         let pledgeForm = document.querySelector('.pledge-form');
         overlay.style.display = 'block';
         pledgeForm.style.display = 'block';
+        if (rewardBtn.classList.contains('bamboo')) {
+            pledgeForm.querySelector('#bamboo-stand').checked = true;
+        } else if (rewardBtn.classList.contains('black')) {
+            pledgeForm.querySelector('#black-edition').checked = true;
+        } else if (rewardBtn.classList.contains('mahogany')) {
+            pledgeForm.querySelector('#mahogany').checked = true;
+        }
     });
 });
 
 overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
     document.querySelector('.pledge-form').style.display = 'none';
+    resetRadioButtons();
 });
+
+fieldsets.forEach(fieldset => {
+    let radio = fieldset.querySelector('input[type="radio"]');
+    radio.addEventListener('change', () => {
+        fieldsets.forEach(fieldset => {
+            fieldset.querySelector('input[type="radio"]').checked = false;
+            fieldset.style.border = '1px solid #eee';
+            let hr = fieldset.querySelector('hr');
+            let pledge = fieldset.querySelector('.pledge');
+            (hr) ? hr.style.display = 'none' : null;
+            (pledge) ? pledge.style.display = 'none' : null;
+            
+        });
+        radio.checked = true;
+        if (fieldset.querySelector('input[type="radio"]').checked) {
+            fieldset.style.border = '2px solid hsl(176, 50%, 47%)';
+            let hr = fieldset.querySelector('hr');
+            let pledge = fieldset.querySelector('.pledge');
+            (hr) ? hr.style.display = 'block' : null;
+            (pledge) ? pledge.style.display = 'flex' : null;
+
+        } 
+    });
+});
+
+

9f845ae5fef1a5038c69359541e62f6f3ad552b0
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-12-31 19:56:49 +0200

    form update

--- project40/script.js
+++ project40/script.js
@@ -21,6 +21,8 @@ const pledgeInputs = document.querySelectorAll('.pledge input[type="number"]');
 const [leftBamboo, leftBlack, leftMahogany, leftBambooForm, leftBlackForm, leftMahoganyForm] = document.querySelectorAll('.remaining-number');
 const left = {'25': [leftBamboo, leftBambooForm], '75': [leftBlack, leftBlackForm], '200': [leftMahogany, leftMahoganyForm]}
 
+
+
 backedCurrent.textContent = `$${backedValue.toLocaleString('en-US')}`;
 backedTotal.textContent = `of $${totalValue.toLocaleString('en-US')} backed`;
 backersTotal.textContent = backers.toLocaleString('en-US');
@@ -40,16 +42,18 @@ function resetRadioButtons(fieldsets) {
 
 radioButtons.forEach(radio => {
     radio.addEventListener('change', () => {
-        pledgeInputs.forEach(input => input.removeAttribute('required')); // Remove required from all
-        const selectedPledge = radio.closest('fieldset').querySelector('input[type="number"]');
-        if (selectedPledge) {
-            selectedPledge.setAttribute('required', 'true'); // Add required to the selected pledge input
+        if (radio.id !== 'no-reward') {
+            pledgeInputs.forEach(input => input.removeAttribute('required'));
+            const selectedPledge = radio.closest('fieldset').querySelector('input[type="number"]');
+            if (selectedPledge) {
+                selectedPledge.setAttribute('required', 'true'); 
+            }
         }
     });
 });
 
 function radioChecked(fieldset) {
-    fieldset.querySelector('button[type="submit"]').required = true;
+    // fieldset.querySelector('button[type="submit"]').required = true;
     fieldset.style.border = '2px solid hsl(176, 50%, 47%)';
     let hr = fieldset.querySelector('hr');
     let pledge = fieldset.querySelector('.pledge');
@@ -57,18 +61,21 @@ function radioChecked(fieldset) {
     (pledge) ? pledge.style.display = 'flex' : null;
 }
 
-pledgeContainers.forEach(pledgeContainer => {
-    let pledgeButton = pledgeContainer.querySelector('button');
-    if (pledgeContainer.querySelector('.remaining-number').textContent === '0') {        
-        pledgeButton.textContent = 'Out of stock';
-        pledgeButton.classList.add('inactive');
-        pledgeContainer.style.opacity = '0.5';
-    } else {
-        pledgeButton.textContent = 'Select Reward';
-        pledgeButton.classList.remove('inactive');
-        pledgeContainer.style.opacity = '1';
-    }
-});
+function checkIfNoLeft() {
+    pledgeContainers.forEach(pledgeContainer => {
+        let pledgeButton = pledgeContainer.querySelector('button');
+        if (pledgeContainer.querySelector('.remaining-number').textContent === '0') {        
+            pledgeButton.textContent = 'Out of stock';
+            pledgeButton.classList.add('inactive');
+            pledgeContainer.style.opacity = '0.5';
+        } else {
+            pledgeButton.textContent = 'Select Reward';
+            pledgeButton.classList.remove('inactive');
+            pledgeContainer.style.opacity = '1';
+        }
+    });
+}
+checkIfNoLeft();
 
 
 rewardBtns.forEach(rewardBtn => {    
@@ -76,7 +83,10 @@ rewardBtns.forEach(rewardBtn => {
         if (rewardBtn.classList.contains('inactive')) return;
         overlay.style.display = 'block';
         pledgeForm.style.display = 'block';
-        if (rewardBtn.classList.contains('bamboo')) {
+        if (rewardBtn.classList.contains('back')) {
+            pledgeForm.querySelector('#no-reward').checked = true;
+            radioChecked(fieldsets[0]);
+        } else if (rewardBtn.classList.contains('bamboo')) {
             pledgeForm.querySelector('#bamboo-stand').checked = true;
             radioChecked(fieldsets[1]);
         } else if (rewardBtn.classList.contains('black')) {
@@ -120,6 +130,7 @@ function updateLeft(id) {
     left[id][0].textContent = parseInt(left[id][0].textContent, 10) - 1;
     console.log(left[id][0]);
     left[id][1].textContent = left[id][0].textContent;
+    checkIfNoLeft();
 }
 
 // Get the data from the selected field and update global info!
@@ -129,7 +140,7 @@ submitBtns.forEach(submitBtn => {
             const input = submitBtn.previousElementSibling;
             valueToCheck = parseInt(input.value, 10);
             treshholdValue = parseInt(input.id, 10);
-            if (valueToCheck < treshholdValue) {
+            if (!input.value || valueToCheck < treshholdValue) {
                 return;
             }
             backedValue += valueToCheck;
@@ -138,6 +149,10 @@ submitBtns.forEach(submitBtn => {
             backersTotal.textContent = backers.toLocaleString('en-US');
             barCurrent.style.width = `${(backedValue/totalValue)*100}%`;
             updateLeft(input.id);
+        } else if (!submitBtn.previousElementSibling)  {
+            backers += 1;
+            backedCurrent.textContent = `$${backedValue.toLocaleString('en-US')}`;            
+            backersTotal.textContent = backers.toLocaleString('en-US');
         }
         
     });

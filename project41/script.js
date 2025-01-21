47da8a32fd6f7c4966d341045e994dfa0f15a56c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-01-21 11:04:00 +0200

    checkbox

--- project41/script.js
+++ project41/script.js
@@ -4,3 +4,14 @@ slider.addEventListener('input', function (e) {
     const value = Number(e.target.value);
     price.textContent = `$${parseFloat(value/100).toFixed(2)}`;
 });
+
+const checkbox = document.querySelector('[type="checkbox"]');
+checkbox.addEventListener('click', function (e) {
+    if (e.target.checked) {
+        e.target.classList.remove('not-checked');
+        e.target.classList.add('checked');
+    } else {
+        e.target.classList.remove('checked');
+        e.target.classList.add('not-checked');
+    }
+});

9ffcfe1691430a9328fba6d46a38eabc3da54462
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-01-22 10:41:15 +0200

    slider

--- project41/script.js
+++ project41/script.js
@@ -1,8 +1,10 @@
 const slider = document.querySelector('.slider');
 const price = document.querySelector('.price');
+const MAXVALUE = 3200;
 slider.addEventListener('input', function (e) {
     const value = Number(e.target.value);
     price.textContent = `$${parseFloat(value/100).toFixed(2)}`;
+    slider.style.background = `linear-gradient(to right, rgb(120,235,222) ${(value/MAXVALUE)*100}%, #f1f1f1 ${(value/MAXVALUE)*100}%)`;
 });
 
 const checkbox = document.querySelector('[type="checkbox"]');

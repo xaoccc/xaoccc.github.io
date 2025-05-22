0c6b80f2c1ee3fd8530239793c2899f4c9f76e12
xaoccc <xaocccc@gmail.com>
2025-05-22 15:20:08 +0300

    comments section

--- project55/script.js
+++ project55/script.js
@@ -1,4 +1,6 @@
 const [comments, newComment, profilePic]  = document.querySelectorAll('.comments, .new-comment, .new-comment > img');
+const commentWrapper = document.querySelector('.comment-wrapper');
+const commentsSection = document.querySelector('section.comments');
 
 
 async function fetchData() {
@@ -7,9 +9,23 @@ async function fetchData() {
     const response = await fetch(request);
     let data = await response.json();
     console.log(data);
+    
 
+    data.comments.forEach(commentData => {
+        let newComment = commentWrapper.cloneNode(true);
+        newComment.querySelector('.profile-pic').src = commentData.user.image.png;
+        newComment.querySelector('.profile-name').textContent = commentData.user.username;
+        newComment.querySelector('.date').textContent = commentData.createdAt;
+        newComment.querySelector('.score').textContent = commentData.score;
+        newComment.querySelector('p').textContent = commentData.content;
+        commentsSection.appendChild(newComment)
+    });
+
+    
     profilePic.src = data.currentUser.image.png;
     const currentUser = data.currentUser.username;
 }
+
+
 
 fetchData();
